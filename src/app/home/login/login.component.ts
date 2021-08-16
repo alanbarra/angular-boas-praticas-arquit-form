ba1a72c7f5b889b53a14569472919cc49da51bfb
alan <alan.ghb@gmail.com>
2021-08-16 11:09:36 -0300

    carga total

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,33 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
+
+  constructor(
+    private authService: AutenticacaoService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('autenticado com sucesso');
+        this.router.navigate(['animais']);
+      },
+      (error) => {
+        alert('usuário ou senha invalido');
+        console.log(error);
+      }
+    );
+  }
+}
